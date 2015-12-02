f83f700d9c6f47dd35b3b46e28c539191c179002
jono-tt <jono-tt@users.noreply.github.com>
2015-12-02 18:55:52 +0000

    Remove all 'null' and 'undefined' entries from output

--- test/custom-yml-reader/yml-include-optional-type-reader.js
+++ test/custom-yml-reader/yml-include-optional-type-reader.js
@@ -11,10 +11,6 @@ suite('Include Files - Optional', function() {
       actual   = null,
       yamlFile = path.resolve(__dirname, 'fixtures/include-optional/include-main-test.yml'),
       expected = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'fixtures/include-optional/include-main-test-expected.json'), 'utf8'));
-
-    //set undefined property as JSON parser doesnt have the undefined for "my-nested-not-exists" which
-    // assert.deepEquals will check against the property being on the object but just as undefined
-    expected["my-nested-not-exists"] = undefined
     
     actual = parser.readYml(yamlFile);
     assert.deepEqual(actual, expected);
